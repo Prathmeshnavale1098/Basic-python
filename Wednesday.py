9a60f3edc2981547893a9b2254a25a3510725d3e
Prathmeshnavale1098 <prathmeshnavale900@gmail.com>
2026-06-04 21:12:29 +0530

    Add files via upload

---
+++ Wednesday.py
@@ -0,0 +1,65 @@
+#write a program to calculate addition,subtraction,multiplication and division of complex number
+'''a=4+2j
+b=6+4j
+print("Addition is=",a+b)
+print("Subtraction is=",a-b)
+print("Multiplication is=",a*b)
+print("Division is=",a/b)'''
+
+#WAP to check 2 object refers to the same memory location or not
+'''a=20
+b=10
+print(id(a),id(b))
+print("a is b=",(a is b))
+print("a is not b=",(a is not b))'''
+
+#WAP to check student name from user and single character check the character is present or not
+'''a=input("Enter a name:")
+b=input("Enter a Charater:")
+print("b in a=",b in a)'''
+
+#WAP to check enter number is multiple of 5 or not
+'''a=int(input("Enter the number:"))
+if(a%5==0):
+    print("It is multiple of 5")
+else:
+    print("It is not multiple of 5")'''
+    
+    
+#WAP to check given number is positive or not
+'''a=int(input("Enter a number:"))
+if (a>=0):
+    print("It is positive")
+else:
+    print("It is not positive number")'''
+
+
+#WAP to take selling price and cost price from user and check if user get profit or loss
+'''a=int(input("Enter a selling price:"))
+b=int(input("Enter a cost price:"))
+if (a>b):
+    print("Profit")
+else:
+    print("Loss")'''
+
+
+#WAP to check given shape is rectangle or square
+'''a=int(input("Enter a length:"))
+b=int(input("Enter a breath:"))
+if(a==b):
+    print("Shape is Square")
+else:
+    print("Shape is Rectangle")'''
+
+
+#WAP to check given no is multiple of 3 or 5
+'''a=int(input("Enter a 1st number:"))
+if(a%3==0 or a%5==0):
+    print("It is multiple of 3 or 5")
+else:
+    print("It is not multiple of 3 or 5")'''
+
+
+'''#WAP to give imoji output using unicode char function and get unicode from user
+a=int(input("Enter a unicode for Imoji"))
+print(chr(a))'''
