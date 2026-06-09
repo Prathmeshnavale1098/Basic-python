e003156365697032c4ed8896756ad4e741d628d9
Prathmeshnavale1098 <prathmeshnavale900@gmail.com>
2026-06-09 12:28:47 +0530

    Patterns
    Tuesday claswork and nested for loop for pattern

---
+++ Nested.py
@@ -0,0 +1,110 @@
+#WAP to print star traingle format
+'''n=4
+for i in range(1,4):
+	print("*"*i)'''
+
+'''for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()'''
+
+'''for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()'''
+
+'''for i in range(4,0,-1):
+    for j in range(i,0,-1):
+        print("*",end="")
+    print()'''
+
+'''for i in range(1,5):
+    for j in range(5,i,-1):
+        print(i,end="")
+    print()'''
+
+'''for i in range(4,0,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()'''
+
+'''n=1
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(n,end=" ")
+        n+=1
+    print()'''
+
+'''n='A'
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(n,end="")
+    print()
+    n=chr(ord(n)+1)'''
+
+'''n='A'
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(n,end="")
+        n=chr(ord(n)+1)
+    print()'''
+    
+'''n='C'
+for i in range(1,4):
+    for j in range(4,i,-1):
+        print(n,end="")
+    print()
+    n=chr(ord(n)-1)'''
+
+'''n=4
+for i in range(1,5):
+    for k in range(n-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()'''
+
+
+'''for i in range(4,0,-1):
+    for k in range(4-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()'''
+
+'''rows = 4
+for i in range(rows):
+    for j in range(rows - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()'''
+    
+'''for i in range(1,5):
+    for j in range(5-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
+    print()
+for i in range(5,-1,-1):
+    for j in range(5-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
+    print()'''
+
+'''n=5
+for i in range (1,n+1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (2*i-1):
+        print("*",end="")
+    print()
+    
+n=5
+for i in range (n,0,-1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (2*i-1):
+        print("*",end="")
+    print()'''
